ecc8a9318eeef2d1d636ada41e49508425f6161e
MD MOHABBAT ALI <mohabbatalit8@gmail.com>
2023-04-09 18:12:21 +0600

    Spinner and Active Link Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Header.css'
 import { Link } from 'react-router-dom';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
     return (
         <div>
             <h1>This is Header components</h1>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/friends">Friends</Link>
+                <ActiveLink to="/">Home</ActiveLink>
+                <ActiveLink to="/about">About</ActiveLink>
+                {/* <Link to="/friends">Friends</Link> */}
+                <ActiveLink to="/friends">Friends</ActiveLink>
                 <Link to='/posts'>Posts</Link>
                 <Link to="/services">Services</Link>
                 <Link to="/contact">Contact</Link>
